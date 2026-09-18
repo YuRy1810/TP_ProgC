6b9d14fd2fc4e49238ce119143407ec49eee01a4
YuRy1810 <nunofilipeteixeiraferreira18@gmail.com>
2026-09-18 09:23:09 +0000

    Exercice 1.10 : pyramide de nombres

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main(void) {
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        // Espaces pour centrer la ligne.
+        for (j = 0; j < n - i; j++) {
+            printf(" ");
+        }
+
+        // Nombres croissants de 1 a i.
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Nombres decroissants de i-1 a 1.
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}

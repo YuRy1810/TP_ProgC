162492c21c04b18865f465032c59277000be5740
YuRy1810 <nunofilipeteixeiraferreira18@gmail.com>
2026-09-18 09:14:06 +0000

    Exercice 1.5 : operateurs arithmetiques et comparaisons

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 16;
+    int b = 3;
+
+    printf("Addition : %d\n", a + b);
+    printf("Soustraction : %d\n", a - b);
+    printf("Multiplication : %d\n", a * b);
+    printf("Division : %d\n", a / b);
+    printf("Modulo : %d\n", a % b);
+    printf("a egal a b : %d\n", a == b);
+    printf("a superieur a b : %d\n", a > b);
+
+    return 0;
+}
 
 

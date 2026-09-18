d0631a3694c8add9d82ba1c73649f4e480377d40
YuRy1810 <nunofilipeteixeiraferreira18@gmail.com>
2026-09-18 09:20:24 +0000

    Exercice 1.8 : calculs avec switch

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,58 @@
+#include <stdio.h>
 
+int main(void) {
+    int num1 = 16;
+    int num2 = 3;
+    char op = '+';
+    int resultat;
+
+    switch (op) {
+        case '+':
+            resultat = num1 + num2;
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Division par zero impossible.\n");
+                return 1;
+            }
+            resultat = num1 / num2;
+            break;
+
+        case '%':
+            if (num2 == 0) {
+                printf("Modulo par zero impossible.\n");
+                return 1;
+            }
+            resultat = num1 % num2;
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            break;
+
+        case '~':
+            resultat = ~num1;
+            break;
+
+        default:
+            printf("Operateur inconnu.\n");
+            return 1;
+    }
+
+    printf("Resultat : %d\n", resultat);
+
+    return 0;
+}
